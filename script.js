58dbbe43e5930c37ad4247bb0406f1035dcbaf92
Garnik11 <https://github.com>
2023-01-19 02:02:13 +0400

    finish navbar

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log(111);
+const data = [
+    {
+        name:"card1",
+        price:"29 900,00",
+        text: "PUFFER JACKET",
+        image: "https://1drv.ms/u/s!Am0OA-IDKu8ujDSSrLLna10iyI_u?e=1buexQ"
+    }
+]
